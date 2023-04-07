5a1e18c9c7eeb630352a9317dbdfb38c7ff90f5e
Mohammad Al-Abedredha <malabedredha@gmail.com>
2023-04-06 23:11:05 -0500

    Created an array for numbers, lowercase and uppercase letters.

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,13 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
