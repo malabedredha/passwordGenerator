f24843808af8e066cfec5aa2bc7697cc80683a5d
Mohammad Al-Abedredha <malabedredha@gmail.com>
2023-04-06 23:47:09 -0500

    Added all 3 of the prompts.

--- Develop/script.js
+++ Develop/script.js
@@ -10,8 +10,16 @@ let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let builtArray = []
 
 function generatePassword() {
-  if (confirm("Would you like your password to contain uppercase letters? ")) {
+  if (confirm("Would you like your password to contain uppercase letters?")) {
     builtArray = [...builtArray, ...upperAlphabet];
+  }
+
+  if (confirm("Would you like your password to contain lowercase letters?")) {
+    builtArray = [...builtArray, ...lowerAlphabet];
+  }
+
+  if (confirm("Would you like your password to contain numbers?")) {
+    builtArray = [...builtArray, ...numbers];
   }
 
 
