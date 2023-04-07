4d982d528c494af2b8e1fc6011a2d8c4d249d2fd
Mohammad Al-Abedredha <malabedredha@gmail.com>
2023-04-06 23:38:47 -0500

    Fixed a few issues in the code.

--- Develop/script.js
+++ Develop/script.js
@@ -3,18 +3,22 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 
-let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function generatePassword() {
-  if (confirm("Would you like your password to contain uppercase letters? "));
-  {
+let builtArray = []
 
+function generatePassword() {
+  if (confirm("Would you like your password to contain uppercase letters? ")) {
+    builtArray = [...builtArray, ...upperAlphabet];
   }
 
-  let password = ""
-  for (let i = 0; i < prompt("How long would you like your password to be"); i++) {
+
+  let password = "";
+
+  let passwordLength = prompt("How long would you like your password to be?")
+  for (let i = 0; i < passwordLength; i++) {
 
     password += "x";
 
